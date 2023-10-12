fd80984fcc116e4736692f58d6a33d079fffb265
Ludvigpbf <ludvig.flyckt@gmail.com>
2023-10-12 23:25:09 +0200

    basic styling dashboard, started on period

--- client/app/(tabs)/[company].tsx
+++ client/app/(tabs)/[company].tsx
@@ -1,65 +1,127 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, FlatList, ScrollView } from "react-native";
+import React, { useState } from "react";
 import { Link, useLocalSearchParams } from "expo-router";
+type Item = {
+  id: number;
+  content: string;
+};
 
 const Dashboard = () => {
   const { company } = useLocalSearchParams();
+
+  const [items, setItems] = useState<Item[]>([
+    { id: 1, content: "Item 1" },
+    { id: 2, content: "Item 2" },
+    { id: 3, content: "Item 3" },
+  ]);
   return (
-    <View style={styles.container}>
-      <Text>{company}'s Dashboard</Text>
-      <View style={styles.btnsContainer}>
-        <Link href="/period/lists/NewList" style={styles.btn}>
-          New List
-        </Link>
-        <Link href="/period/lists/NewList" style={styles.btn}>
-          New List
-        </Link>
-        <Link href="/period/lists/NewList" style={styles.btn}>
-          New List
-        </Link>
-        <Link href="/period/lists/NewList" style={styles.btn}>
-          New List
-        </Link>
-        <Link href="/period/lists/NewList" style={styles.btn}>
-          New List
-        </Link>
-        <Link href="/period/lists/NewList" style={styles.btn}>
-          New List
-        </Link>
-        <Link href="/period/lists/NewList" style={styles.btn}>
-          New List
-        </Link>
-        <Link href="/period/lists/NewList" style={styles.btn}>
-          New List
-        </Link>
-      </View>
-      <View style={styles.container2}>
-        <Text>Some Information</Text>
-        <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, iusto!
-        </Text>
-      </View>
-      <View style={styles.container4}>
-        <View style={styles.container3}>
-          <Text>Some Information</Text>
-          <Text></Text>
+    <ScrollView contentContainerStyle={styles.containerColumn}>
+      <View style={styles.containerRow}>
+        <Text>{company}'s Dashboard</Text>
+        <View style={styles.btnsContainer}>
+          <Link href="/period/lists/NewList" style={styles.btn}>
+            <Text style={styles.btnText}>New List</Text>
+          </Link>
+          <Link href="/period/lists/NewList" style={styles.btn}>
+            <Text style={styles.btnText}>New List</Text>
+          </Link>
+          <Link href="/period/lists/NewList" style={styles.btn}>
+            <Text style={styles.btnText}>New List</Text>
+          </Link>
+          <Link href="/period/lists/NewList" style={styles.btn}>
+            <Text style={styles.btnText}>New List</Text>
+          </Link>
+          <Link href="/period/lists/NewList" style={styles.btn}>
+            <Text style={styles.btnText}>New List</Text>
+          </Link>
+          <Link href="/period/lists/NewList" style={styles.btn}>
+            <Text style={styles.btnText}>New List</Text>
+          </Link>
+          <Link href="/period/lists/NewList" style={styles.btn}>
+            <Text style={styles.btnText}>New List</Text>
+          </Link>
+          <Link href="/period/lists/NewList" style={styles.btn}>
+            <Text style={styles.btnText}>New List</Text>
+          </Link>
         </View>
-        <View style={styles.container3}>
+        <View style={styles.container2}>
           <Text>Some Information</Text>
-          <Text></Text>
+          <Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+            iusto!
+          </Text>
+        </View>
+        <View style={styles.flatlistContainer}>
+          <FlatList
+            data={items}
+            horizontal={true}
+            renderItem={({ item }) => (
+              <View style={styles.container3}>
+                <Text>Some Information</Text>
+                <Text>{item.content}</Text>
+              </View>
+            )}
+            keyExtractor={(item) => item.id.toString()}
+          />
+        </View>
+        <View style={[styles.containerColumn, styles.background]}>
+          <Link
+            href="/(tabs)/statistics/Statistic"
+            style={styles.containerColumn}
+          >
+            <Text style={styles.title}>Last Period:</Text>
+            <View>
+              <Text>Section one</Text>
+              <Text>Section two</Text>
+              <Text>Section three</Text>
+              <Text>Section four</Text>
+            </View>
+          </Link>
+        </View>
+        <View style={[styles.containerColumn, styles.background]}>
+          <Link
+            href="/(tabs)/statistics/Statistic"
+            style={styles.containerColumn}
+          >
+            <Text style={styles.title}>Last Period:</Text>
+            <View>
+              <Text>Section one</Text>
+              <Text>Section two</Text>
+              <Text>Section three</Text>
+              <Text>Section four</Text>
+            </View>
+          </Link>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
 export default Dashboard;
 
 const styles = StyleSheet.create({
-  container: {
+  containerRow: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    marginTop: 20,
+  },
+  containerColumn: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  background: {
+    backgroundColor: "#D9D9D9",
+    height: 200,
+    borderRadius: 10,
+    width: 330,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
   btnsContainer: {
     display: "flex",
@@ -77,6 +139,14 @@ const styles = StyleSheet.create({
     width: 60,
     backgroundColor: "gray",
     margin: 10,
+    borderRadius: 50,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btnText: {
+    textAlign: "center",
+    /*  textAlignVertical: "center", */
   },
   container2: {
     backgroundColor: "#D9D9D9",
@@ -87,10 +157,12 @@ const styles = StyleSheet.create({
   },
   container3: {
     backgroundColor: "#D9D9D9",
-    width: 150,
+    width: 200,
     borderRadius: 10,
-
+    height: 60,
     padding: 10,
+    marginTop: 20,
+    marginLeft: 30,
   },
   container4: {
     display: "flex",
@@ -99,5 +171,14 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     width: 330,
     marginTop: 20,
+  },
+  flatlistContainer: {
+    flexDirection: "row",
+  },
+  statsContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- client/app/(tabs)/period/index.tsx
+++ client/app/(tabs)/period/index.tsx
@@ -1,15 +1,59 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
 
 const NewPeriod = () => {
+  const [periodName, setPeriodName] = useState("");
+  const historyTexts = ["Januari", "Februari", "Mars"];
+
+  const handleSubmit = () => {
+    // Handle the submit action here. You can use the periodName state.
+    console.log("Submitted period name:", periodName);
+  };
   return (
     <>
-      <View style={styles.container}>
-        <Text>NewPeriod</Text>
-      </View>
-      <View style={styles.container}>
-        <Text>History</Text>
-      </View>
+      <ScrollView contentContainerStyle={styles.containerColumn}>
+        <View style={[styles.container, styles.bg, styles.newPeriodContainer]}>
+          <Text>NewPeriod</Text>
+          <View style={[styles.bg, styles.calenderContainer]}>
+            <Text>Choose period</Text>
+            <View style={[styles.bg, styles.calender]}>
+              <View>
+                <Text>Calender</Text>
+              </View>
+            </View>
+          </View>
+          <View>
+            <Text>Name period:</Text>
+            <TextInput
+              style={styles.input}
+              value={periodName}
+              onChangeText={(text) => setPeriodName(text)}
+              placeholder="Enter period name"
+            />
+          </View>
+          <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+            <Text style={styles.buttonText}>Submit</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.containerColumn}>
+          <Text>History</Text>
+          {historyTexts.map((text, index) => (
+            <View
+              key={index}
+              style={[styles.containerColumn, styles.bg, styles.historyButton]}
+            >
+              <Text>{text}</Text>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
     </>
   );
 };
@@ -21,5 +65,52 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    width: 330,
+  },
+  containerColumn: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  bg: {
+    backgroundColor: "#D9D9D9",
+  },
+  newPeriodContainer: {
+    height: 200,
+  },
+  calenderContainer: {
+    height: 100,
+
+    width: 250,
+    padding: 10,
+  },
+  calender: {
+    height: 70,
+    backgroundColor: "white",
+  },
+  input: {
+    height: 25,
+    width: 150,
+    borderColor: "gray",
+    borderWidth: 1,
+    paddingLeft: 5,
+    marginTop: 5,
+  },
+  button: {
+    backgroundColor: "#007BFF",
+    borderRadius: 5,
+    padding: 10,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  historyButton: {
+    padding: 10,
+    width: 330,
+    borderRadius: 10,
   },
 });
